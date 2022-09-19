40e04033681c8f73b36a05c5f1b95f2b10ffbdba
pchchv <ipchchv@gmail.com>
2022-09-19 11:16:44 +0300

    Implement a server

---
+++ src/service/server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func server() {
+	e := echo.New()
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(1000)))
+	log.Fatal(e.Start(":" + getEnvValue("PORT")))
+}
