a123b54228e22b35aeb228c2b5554d76482b88da
pchchv <ipchchv@gmail.com>
2022-09-21 15:55:34 +0300

    Remove a getAllAds handler

--- service/server.go
+++ service/server.go
@@ -23,19 +23,11 @@ func subscribeHandler(c echo.Context) error {
 	return c.String(http.StatusOK, message)
 }
 
-func getAllAdsHandler(c echo.Context) error {
-	user := c.QueryParam("user")
-	getAllAds(user)
-	message := ""
-	return c.String(http.StatusOK, message)
-}
-
 // The declaration of all routes comes from it
 func routes(e *echo.Echo) {
 	e.GET("/", pingHandler)
 	e.GET("/ping", pingHandler)
 	e.POST("/subscribe", subscribeHandler)
-	e.GET("/allads", getAllAdsHandler)
 }
 
 func server() {
